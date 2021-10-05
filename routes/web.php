dc49b207a0820db76076f64288320fb8e225d53d
Jhilm <jnukuravuisente28@gmail.com>
2021-10-04 22:08:06 -0400

    restore configuration

--- routes/web.php
+++ routes/web.php
@@ -12,9 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-/*
+
 Route::get('/', function () {
     return view('welcome');
 });
-*/
-Route::get('/',[nuevoControlador::class,"inicio"]);
+
+//use app/Http
+
+//Route::get('/',[nuevoControlador::class,"inicio"]);
