29e0cf1ebd6dcb92b564e51e605114ab5383a3a1
Jhilm <jnukuravuisente28@gmail.com>
2021-10-13 18:52:41 -0400

    init

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-<<<<<<< HEAD
-Route::get('/RegistroGE', function(){
-    return view('registroGE');
-});
-=======
-//use app/Http
-
-//Route::get('/',[nuevoControlador::class,"inicio"]);
->>>>>>> dc49b207a0820db76076f64288320fb8e225d53d
