a191b59a7f6e673eebcc1036731987cb813f7f2e
adrianC56 <adriancrespog3@gmail.com>
2021-12-07 00:21:05 -0400

    Solo estudiantes en Inscritos

--- app/Http/Controllers/VistaInscritosController.php
+++ app/Http/Controllers/VistaInscritosController.php
@@ -14,11 +14,15 @@ class VistaInscritosController extends Controller
     }
 
     public function getUsuariosMismoGrupo(Request $request){
+
+        $rolEst = DB::table('Rol')->where('nombreRol','=','Estudiante')->first();
+
         $usuario = Usuario::findOrFail($request->idUsuario);
         $usuarios = DB::table('Usuario')
                     ->leftjoin('Grupo_Empresa','Grupo_Empresa.idGE','=','Usuario.idGE')
                     ->where('Usuario.idGrupo','=',$usuario->idGrupo)
                     ->where('Usuario.registrado','=',true)
+                    ->where('Usuario.idRol','=',$rolEst->idRol)
                     ->orderBy('Usuario.idUsuario','ASC')
                     ->get();
         return response()->json($usuarios);
